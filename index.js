bf78fab8bac802f39b6a4f9fc58f84bbf1c96d83
Courtney Carroll <bcakescw@gmail.com>
2022-03-28 20:19:11 -0400

    Append it to the Content Container

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 let content = document.querySelector('.js-generated.content')
 let header = document.createElement (h1)
+
 header.setAttribute("class", "dog-name")
+
 header.append ("Rizzo")
+
+content.append(header)
